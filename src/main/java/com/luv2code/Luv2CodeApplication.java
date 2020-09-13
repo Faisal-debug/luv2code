315870b411489f8218acb5b3e034a7326b26246c
Faisal-debug <faisal3000khan@gmail.com>
2020-09-13 15:37:04 +0530

    initial commit

--- src/main/java/com/luv2code/Luv2CodeApplication.java
+++ src/main/java/com/luv2code/Luv2CodeApplication.java
@@ -2,14 +2,17 @@ package com.luv2code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 //@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-public class Luv2CodeApplication {
-
+public class Luv2CodeApplication{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Luv2CodeApplication.class, args);
 	}
+
+
+	
 
 }
